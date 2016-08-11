3f05d0b20d011836b5c2029f2110037a5f5d05f6
Andra Matei <andmatei@endava.com>
2016-08-11 11:54:23 +0300

    another push

---
+++ wikiweb/src/main/java/model/CountWords.java
@@ -0,0 +1,65 @@
+package model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
+
+/**
+ * Created by andmatei on 8/11/2016.
+ */
+public class CountWords {
+    protected static void countWords() {
+        Map<String, Integer> wordCount = new LinkedHashMap<>();
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("fisierXML.txt"));
+            String line;
+            while ((line = in.readLine()) != null) {
+                line = line.toUpperCase(); // convert to lower case
+                String[] words = line.split("\\s+"); //split the line on whitespace, would return an array of words
+                for (String word : words) {
+                    if (word.length() == 0) {
+                        continue;
+                    }
+                    Integer occurences = wordCount.get(word);
+                    if (occurences == null) {
+                        occurences = 1;
+                    } else {
+                        occurences++;
+                    }
+                    if (!isPreposition(word)) {
+                        wordCount.put(word, occurences);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Exception");
+        }
+
+        List<Integer> values = new ArrayList<>();
+        values.addAll(wordCount.values());
+        Collections.sort(values, Collections.reverseOrder());
+        int last_i = -1;
+
+
+        for (Integer i : values.subList(0, 9)) {
+            if (last_i == i) // without duplicates
+                continue;
+            last_i = i;
+
+            for (String s : wordCount.keySet()) {
+                if (wordCount.get(s) == i) // which have this value
+                    System.out.println(s + " " + i);
+            }
+
+        }
+    }
+
+    private static boolean isPreposition(String w) {
+        for (PrepositionsEnum preposition : PrepositionsEnum.values()) {
+            if (w.equals(preposition.toString())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ wikiweb/src/main/java/model/WriteFileFromXML.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by andmatei on 8/11/2016.
+ */
+public class WriteFileFromXML {
+    public static void writeFileFromXML() {
+        URL url = null; //Reading
+        try {
+            url = new URL("https://en.wikipedia.org/wiki/Special:Export?pages=Java");
+            URLConnection urlConnection = url.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            BufferedWriter out = new BufferedWriter(new FileWriter("fisierXML.txt"));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                out.write(inputLine);
+                out.newLine();
+            }
+            in.close();
+            out.close();
+
+        } catch (MalformedURLException e) {
+            System.err.println("malformedException");
+        } catch (IOException e) {
+            System.err.println("ioexception");
+        }
+    }
+}
